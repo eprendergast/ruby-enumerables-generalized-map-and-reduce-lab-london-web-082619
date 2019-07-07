971825dec2ee5d795a472a0a1e7d74f0630fa04a
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-07 18:54:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,12 +9,12 @@ def map(source_array)
     return new
 end
 
-def reduce(source_array, starting_point = 0)
-  running_total = starting_point
-  i = 0
-  while i < source_array.length do
-    yield(running_total, source_array[i])
-    i += 1
-  end
-  return running_total
-end
+# def reduce(source_array, starting_point = 0)
+#   running_total = starting_point
+#   i = 0
+#   while i < source_array.length do
+#     yield(running_total, source_array[i])
+#     i += 1
+#   end
+#   return running_total
+# end
